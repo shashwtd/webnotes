c32cc5202ce19563feabd6eac3a828737816df4c
Shashwat Dubey <shashwat5590@gmail.com>
2025-07-21 06:20:35 +0530

    Creating the cool header component, removed the unnecessary files

---
+++ src/components/Header.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import classNames from "classnames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links: {
+    href: string;
+    label: string;
+}[] = [
+    {
+        href: "/",
+        label: "Home",
+    },
+    {
+        href: "/explore",
+        label: "Explore",
+    },
+    {
+        href: "/share",
+        label: "Share",
+    },
+];
+
+export default function Header() {
+    const path = usePathname();
+
+    return (
+        <div className="fixed top-0 w-screen h-16 bg-neutral-200 px-12 flex items-center justify-between">
+            <Link
+                href="/"
+                className="group text-lg font-mono font-semibold text-neutral-900 flex items-center justify-center gap-0.5"
+            >
+                <span>RGX</span>
+                <span className="duration-200 group-hover:w-6 h-0.5 w-2 bg-black"></span>
+                <span>2287</span>
+            </Link>
+            <nav className="flex space-x-8">
+                {links.map((link, i) => {
+                    const isActive = path === link.href;
+                    return (
+                        <Link
+                            key={i}
+                            href={link.href}
+                            className={`group flex items-center justify-center gap-1 ${
+                                isActive
+                                    ? "text-neutral-900"
+                                    : "text-neutral-600 hover:text-neutral-700"
+                            }`}
+                        >
+                            <span className={classNames("text-sm group-hover:opacity-50",
+                                isActive ? "opacity-100!" : "opacity-0"
+                            )}>
+                                ●
+                            </span>
+                            <span className="tracking-tight lowercase font-semibold font-mono">
+                                {link.label}
+                            </span>
+                        </Link>
+                    );
+                })}
+            </nav>
+        </div>
+    );
+}
