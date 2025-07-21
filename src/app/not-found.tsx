c342811451aaeb3fce4552bdf2934257738128be
Shashwat Dubey <shashwat5590@gmail.com>
2025-07-21 06:51:21 +0530

    Add NotFound component with 404 message layout

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <main className="w-screen h-screen flex-col gap-2 flex items-center justify-center">
+        <h1 className="font-mono text-2xl font-semibold">404</h1>
+        <h1 className="font-mono text-sm tracking-tight font-medium opacity-75">is this even real?</h1>
+      </main>
+    );
+}
