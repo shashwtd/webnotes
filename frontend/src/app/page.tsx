ba74039cbe11dd9306f2d629e91c1f4307f42096
Shashwat Dubey <shashwat5590@gmail.com>
2025-07-21 15:21:57 +0530

    Updated homepage to include a hero section about webnotes. Added CTA to the header

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,34 @@
+import { ChevronRight } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
     return (
-        <main className="w-screen h-screen gap-2 flex items-center justify-center">
-            <section className="w-full h-screen flex flex-col items-center justify-center">
-                <h1 className="font-mono text-2xl font-semibold">
-                    THE MODERN WRLD SHOPPING EXPERIENCE
+        <main className="w-screen h-screen gap-2 bg-neutral-100">
+            <section className="w-full h-screen flex flex-col items-center justify-center gap-2">
+                <h1 className="font-sans text-5xl font-semibold tracking-tighter opacity-60">
+                    Wanna create a Journal?
                 </h1>
-                <p className="font-mono text-sm tracking-tight font-medium opacity-75">
-                    {/* Explore the universe of RGX 2287. */}
+                <h1 className="font-sans text-5xl font-semibold tracking-tighter opacity-90">
+                    It&apos;s done easy with Webnotes
+                </h1>
+                <p className="font-sans text-lg  max-w-lg mt-2 text-center font-medium opacity-75">
+                    Webnotes is a simple, open-source journalling companion app
+                    that helps you sync your notes to the internet. The
                 </p>
+                <div className="flex items-center justify-center mt-6 gap-3">
+                    <Link
+                        href="/login"
+                        className="flex items-center justify-center gap-2 bg-gradient-to-b from-neutral-300/60 to-neutral-400/30 rounded-full text-neutral-800 font-medium font-sans px-6 py-2 hover:to-neutral-400/50 duration-200 transition-colors"
+                    >
+                        Learn More 
+                    </Link>
+                    <Link
+                        href="/login"
+                        className="group flex font-sans font-medium items-center justify-center gap-2 bg-gradient-to-b from-blue-500 hover:from-blue-600 duration-200 to-blue-700 rounded-full text-white px-6 py-2 transition-colors"
+                    >
+                        Get Started <ChevronRight className="-mr-1 mb-0.5 group-hover:translate-x-1 duration-200" size={20}/>
+                    </Link>
+                </div>
             </section>
         </main>
     );
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,22 +3,23 @@
 import classNames from "classnames";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { ChevronRight } from "lucide-react";
 
 const links: {
     href: string;
     label: string;
 }[] = [
     {
-        href: "/",
-        label: "Home",
+        href: "/about",
+        label: "about",
     },
     {
-        href: "/explore",
-        label: "Explore",
+        href: "/get-started",
+        label: "guide",
     },
     {
-        href: "/share",
-        label: "Share",
+        href: "/login",
+        label: "login",
     },
 ];
 
@@ -26,40 +27,74 @@ export default function Header() {
     const path = usePathname();
 
     return (
-        <div className="fixed top-0 w-screen h-max px-6 md:px-12 flex items-start pt-4 justify-between">
-            <Link
-                href="/"
-                className="group text-lg font-mono font-semibold text-neutral-900 flex items-center justify-center gap-0.5"
-            >
-                <span>RGX</span>
-                <span className="duration-200 group-hover:w-6 h-0.5 w-2 bg-black"></span>
-                <span>2287</span>
-            </Link>
-            <nav className="flex flex-col md:flex-row gap-0 md:gap-8 items-start">
-                {links.map((link, i) => {
-                    const isActive = path === link.href;
-                    return (
-                        <Link
-                            key={i}
-                            href={link.href}
-                            className={`group flex items-center justify-center gap-1 ${
-                                isActive
-                                    ? "text-neutral-900"
-                                    : "text-neutral-600 hover:text-neutral-700"
-                            }`}
-                        >
-                            <span className={classNames("text-sm group-hover:opacity-50",
-                                isActive ? "opacity-100!" : "opacity-0"
-                            )}>
-                                ●
+        <div className="fixed top-0 w-screen h-max px-6 md:px-12 flex items-start pt-4 justify-center">
+            <div className="w-full flex items-start justify-between">
+                <Link
+                    href="/"
+                    className="group text-lg font-mono font-medium tracking-tight text-neutral-900 flex items-center justify-center gap-1"
+                >
+                    <span>notes</span>
+                    <div className="flex items-center translate-y-0.5 justify-center">
+                        <span className="duration-200 group-hover:w-8 h-0.5 w-3 bg-black ml-1.5"></span>
+                        <ChevronRight className="-ml-3 left-0 size-5 text-neutral-900 opacity-0 group-hover:opacity-100" />
+                    </div>
+                    <span>web</span>
+                </Link>
+                <nav className="flex flex-col md:flex-row gap-0 md:gap-4 items-start md:items-center">
+                    {links.map((link, i) => {
+                        const isActive = path === link.href;
+                        return (
+                            <Link
+                                key={i}
+                                href={link.href}
+                                className={`group flex items-center justify-center gap-2 ${
+                                    isActive
+                                        ? "text-neutral-900 pointer-events-none"
+                                        : "text-neutral-600 hover:text-neutral-700"
+                                }`}
+                            >
+                                <span
+                                    className={classNames(
+                                        "text-sm group-hover:opacity-50",
+                                        isActive ? "opacity-100!" : "opacity-0"
+                                    )}
+                                >
+                                    ●
+                                </span>
+                                <div className="tracking-tight lowercase font-semibold font-mono relative w-max h-max overflow-hidden">
+                                    <span className="opacity-0">
+                                        {link.label}
+                                    </span>
+                                    <span className="absolute left-0 top-0 group-hover:-top-full group-hover:opacity-0 duration-300 delay-50">
+                                        {link.label}
+                                    </span>
+                                    <span className="absolute left-0 top-full group-hover:top-0 duration-300 delay-50">
+                                        {link.label}
+                                    </span>
+                                </div>
+                            </Link>
+                        );
+                    })}
+                    <Link
+                        href="/login"
+                        className="group ml-4 flex items-center font-sans font-medium text-smitems-center justify-center gap-2 bg-neutral-200 rounded-full text-neutral-900 px-6 py-1.5 pb-2 transition-colors"
+                    >
+                        <div className="relative w-max h-max overflow-hidden">
+                            <span className="opacity-0">register</span>
+                            <span className="absolute left-0 top-0 group-hover:-top-full group-hover:opacity-0 duration-300 delay-50">
+                                register
                             </span>
-                            <span className="tracking-tight lowercase font-semibold font-mono">
-                                {link.label}
+                            <span className="absolute left-0 top-full group-hover:top-0 duration-300 delay-50">
+                                register
                             </span>
-                        </Link>
-                    );
-                })}
-            </nav>
+                        </div>
+                        <ChevronRight
+                            className="-mr-1 group-hover:translate-x-1 duration-200"
+                            size={16}
+                        />
+                    </Link>
+                </nav>
+            </div>
         </div>
     );
 }
